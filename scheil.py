3c48bbde188912343d67473ff3a6fdaa9aadb453
T_cat9000_2 <terpstragraham@gmail.com>
2025-10-13 16:32:43 -0700

    Add files via upload

--- scheil.py
+++ scheil.py
@@ -7,8 +7,8 @@ import argparse
 import os
 import subprocess
 # grab file from argparse
-argparser = argparse.ArgumentParser(description="Scheil lexer")
-argparser.add_argument("file", help="File to lex")
+argparser = argparse.ArgumentParser(description="Scheil")
+argparser.add_argument("file", help="File to compile")
 args = argparser.parse_args()
 file = args.file
 file = rf"{file}" # avoid raw file mixups and stuff.
@@ -18,12 +18,14 @@ if not os.path.isfile(file):
     print(f"File {file} does not exist.")
     exit(1)
 print("Compiling your program...")
-subprocess.run(["python3", "src/lexer.py", file])
-subprocess.run(["python3", "src/parser.py", file])
-subprocess.run(["python3", "src/compiler.py", file])
+subprocess.run(["python", "src/lexer.py", file])
+subprocess.run(["python", "src/parser.py", file])
+# Convert .sc to .scir for the compiler
+scir_file = file.replace('.sc', '.scir')
+subprocess.run(["python", "src/compiler.py", scir_file, "--direct"])
 # cleanup
 file = file[:-3]
 os.remove(f"{file}.sctk")
 os.remove(f"{file}.scir")
 os.remove(f"{file}.ll")
-print(f"Done! Your output file is {file}.sc")
+os.remove(f"{file}.s")
